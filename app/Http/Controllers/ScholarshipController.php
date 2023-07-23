b8faf30662b6002b14d266286500675b806587da
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-07-24 03:21:16 +0530

    update

--- app/Http/Controllers/ScholarshipController.php
+++ app/Http/Controllers/ScholarshipController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class ScholarshipController extends Controller
 {
@@ -15,6 +16,7 @@ class ScholarshipController extends Controller
      */
     public function index()
     {
+        dd(Auth::id());
         return view('form');
     }
 
