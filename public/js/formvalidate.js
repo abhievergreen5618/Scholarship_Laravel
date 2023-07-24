ea80996d55a1049860493ece6a50b3b6a079a4ba
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-07-24 21:58:30 +0530

    Update formvalidate.js

--- public/js/formvalidate.js
+++ public/js/formvalidate.js
@@ -2,24 +2,24 @@
 
 jQuery('#frm').validate({
     rules:{
-        // name:"required",
-        // fathername:"required",
-        // mothername:"required",
-        // examcentre:"required",
-        // caddress:"required",
-        // mobileno:{
-        //     required:true,
-        //     maxlength:10
-        // },
-        // paddress:"required",
-        // email:{
-        //     required:true,
-        //     email:true
-        // },
-        // hsmarksheetmatric:"required",
-        // hsmarksheet:"required",
-        // applyingfor:"required",
-        // category:"required"
+        name:"required",
+        fathername:"required",
+        mothername:"required",
+        examcentre:"required",
+        caddress:"required",
+        mobileno:{
+            required:true,
+            maxlength:10
+        },
+        paddress:"required",
+        email:{
+            required:true,
+            email:true
+        },
+        hsmarksheetmatric:"required",
+        hsmarksheet:"required",
+        applyingfor:"required",
+        category:"required"
     },
     messages:{
         examcentre:"Select an option",
