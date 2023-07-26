dd4f493f05eb10b3d02e6ef0560d5c7064263de5
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-07-26 17:52:56 +0530

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -11,9 +11,41 @@ $(document).ready(function () {
     });
 
     $("#sign_photo").change(function (e) {
-        console.log($(this).files);
+        // Get the selected file
+        var file = e.target.files[0];
+
+        // Check if a file is selected
+        if (file) {
+            // Check the file size in KB
+            var fileSizeKB = file.size / 1024;
+            if (fileSizeKB > 20) {
+                // File size exceeds 20KB, do something (e.g., show an error message)
+                console.log("File size exceeds 20KB");
+                // You can show an error message to the user or handle the case accordingly
+            } else {
+                // File size is within the limit, proceed with your logic
+                console.log("File size is within the limit");
+                // You can proceed with your logic here, e.g., upload the file or process it further
+            }
+        }
     });
     $("#profile_photo").change(function (e) {
-        console.log($(this).files);
+        // Get the selected file
+        var file = e.target.files[0];
+
+        // Check if a file is selected
+        if (file) {
+            // Check the file size in KB
+            var fileSizeKB = file.size / 1024;
+            if (fileSizeKB > 20) {
+                // File size exceeds 20KB, do something (e.g., show an error message)
+                console.log("File size exceeds 20KB");
+                // You can show an error message to the user or handle the case accordingly
+            } else {
+                // File size is within the limit, proceed with your logic
+                console.log("File size is within the limit");
+                // You can proceed with your logic here, e.g., upload the file or process it further
+            }
+        }
     });
 });
