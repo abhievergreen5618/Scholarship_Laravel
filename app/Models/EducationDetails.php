c3bf9f16150616e5fb2dec5ac24f5f82eef3d2d3
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-07-28 00:24:02 +0530

    updated code

--- app/Models/EducationDetails.php
+++ app/Models/EducationDetails.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class EducationDetails extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'resultstatus',
+        'examination_passed',
+        'name_of_the_board_university',
+        'passing_year',
+        'credits_marks_Obtained',
+        'maximum_marks',
+        'percentage_marks',
+        'exam_roll_no',
+        'disqualified/suspended',
+        'disqualified/suspended_details',
+        'type'
+    ];
 }
