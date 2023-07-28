5c09c9d0ba99500560931613d390339e8075f584
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-07-28 18:55:22 +0530

    Update personalinfo.blade.php

--- resources/views/ScholarshipFormSteps/personalinfo.blade.php
+++ resources/views/ScholarshipFormSteps/personalinfo.blade.php
@@ -138,7 +138,7 @@
                                 </td>
                                 <td class="colon">:</td>
                                 <td>
-                                    <span id="Anthem_ctl00_ContentPlaceHolder1_txtDob__"><input type="date" value="{{!empty(auth()->user()->step1_updated_at) ? date('m/d/Y',strtotime((auth()->user()->dob))) : ''}}" id="dob" name="dob" class="form-control">
+                                    <span id="Anthem_ctl00_ContentPlaceHolder1_txtDob__"><input type="date" value="{{!empty(auth()->user()->step1_updated_at) ? date('d/m/Y',strtotime((auth()->user()->dob))) : ''}}" id="dob" name="dob" class="form-control">
                                 </td>
                             </tr>
                             <tr>
