69c7d4414a6c75b144ce986a3d9b159a2a2e61ff
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-07-29 18:18:29 +0530

    Update personalinfo.blade.php

--- resources/views/ScholarshipFormSteps/personalinfo.blade.php
+++ resources/views/ScholarshipFormSteps/personalinfo.blade.php
@@ -147,7 +147,7 @@
                                 </td>
                                 <td class="colon">:</td>
                                 <td>
-                                    <span id="Anthem_ctl00_ContentPlaceHolder1_txtAadhaarNo__"><input name="aadhaarno" id="aadhaarno" class="form-control" type="number" value="{{!empty(auth()->user()->step1_updated_at)) ? auth()->user()->aadhaarno : ''}}" maxlength="12" ondrop="return false;" ondrag="return false;" class="textboxlong"></span>
+                                    <span id="Anthem_ctl00_ContentPlaceHolder1_txtAadhaarNo__"><input name="aadhaarno" id="aadhaarno" class="form-control" type="number" value="{{!empty(auth()->user()->step1_updated_at) ? auth()->user()->aadhaarno : ''}}" maxlength="12" ondrop="return false;" ondrag="return false;" class="textboxlong"></span>
                                 </td>
                             </tr>
                             <tr>
