7776d9cb2795d9df0b61e50aa4c66c9463c2aa35
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-07-31 18:04:59 +0530

    Update personalinfo.blade.php

--- resources/views/ScholarshipFormSteps/personalinfo.blade.php
+++ resources/views/ScholarshipFormSteps/personalinfo.blade.php
@@ -34,7 +34,7 @@
                                 </td>
                                 <td class="colon">:</td>
                                 <td>
-                                    <span id="Anthem_ctl00_ContentPlaceHolder1_txtName__"><input name="name" type="text" value="{{auth()->user()->name}}" readonly maxlength="50" id="name" class="textboxlong form-control" style="text-transform: uppercase"></span>
+                                    <span id="Anthem_ctl00_ContentPlaceHolder1_txtName__"><input name="name" type="text" value="{{isset(auth()->user()->name) ? auth()->user()->name : ''}}" {{isset(auth()->user()->name) ? 'readonly' : ''}} maxlength="50" id="name" class="textboxlong form-control" style="text-transform: uppercase"></span>
                                 </td>
                             </tr>
                             <tr>
