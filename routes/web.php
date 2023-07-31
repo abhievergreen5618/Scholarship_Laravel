fc912e96e9673738acf2661767bf733e3928e094
shivangi980 <shivangidhiman980@gmail.com>
2023-07-31 17:18:42 +0530

    mobile_login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ValidateController;
+use App\Http\Controller\Auth\RegisterController;
+use App\Http\Controllers\Auth\AuthOtpController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +20,16 @@ Route::get('/', function () {
     return view('form');
 });
 
-
+Route::get('/register', function () {
+    return view('auth.register');
+});
 Route::post('my-form', [ValidateController::class, 'myformPost'])->name('my.form');
 
+Auth::routes();
 
-Route::get('register', [OtpController::class, 'register_form']);
+Route::controller(App\Http\Controllers\Auth\AuthOtpController::class)->group(function(){
+    Route::get('otp/login', 'login')->name('otp.login');
+    Route::post('otp/generate', 'generate')->name('otp.generate');
+    Route::get('otp/verification/{user_id}', 'verification')->name('otp.verification');
+    Route::post('otp/login', 'loginWithOtp')->name('otp.getlogin');
+});
