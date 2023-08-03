d7104d835147fe144c0c3f65dc536c91695472bf
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-08-04 03:14:11 +0530

    update

--- database/migrations/2023_08_03_123101_create_state_models_table.php
+++ database/migrations/2023_08_03_123101_create_state_models_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('state_models', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('state_id');
+            $table->string('code', 2)->unique();
             $table->timestamps();
         });
     }
