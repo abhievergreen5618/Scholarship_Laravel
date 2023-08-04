1fd0af8d96eece141d1ceb4c8f56df761e9e6043
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-08-04 17:32:37 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -3,19 +3,6 @@
 @section('content')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">General Form</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
 
     <!-- Main content -->
     <section class="content">
@@ -26,7 +13,7 @@
                     <!-- general form elements -->
                     <div class="card card-primary">
                         <div class="card-header">
-                            <h3 class="card-title">Quick Example</h3>
+                            <h3 class="card-title">Add Class</h3>
                         </div>
                         <div class="card-body">
                             <div class="form-group">
