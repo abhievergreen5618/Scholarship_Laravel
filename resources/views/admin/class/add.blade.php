a60f9a2a626471a19ffabefde87575a042971b98
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-08-04 17:29:34 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -7,10 +7,7 @@
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>General Form</h1>
-                </div>
-                <div class="col-sm-6">
+                <div class="col-sm-12">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active">General Form</li>
@@ -31,8 +28,6 @@
                         <div class="card-header">
                             <h3 class="card-title">Quick Example</h3>
                         </div>
-
-
                         <div class="card-body">
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Email address</label>
