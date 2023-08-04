b4b653c10eb28c41d9ec9317387c47c0157202a9
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-08-04 17:39:38 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -4,7 +4,7 @@
 <div class="col-md-12">
     <div class="card card-primary">
         <div class="card-header">
-            <h3 class="card-title">{{ __('Add Inspection Type') }}</h3>
+            <h3 class="card-title">{{ __('Add Class') }}</h3>
         </div>
         <!-- /.card-header -->
         <!-- form start -->
