2014beecd62f37388406be03bb5309db85620c51
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-08-07 14:24:06 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -16,13 +16,42 @@
             <div class="card-body">
                 <div>
                     <div class="form-group">
-                        <label>Select</label>
-                        <select class="form-control">
+                        <label>Class</label>
+                        <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="class">
                             <option>Select Class</option>
-                            @for($i=1;$i<=12;$i++)
-                                <option value="{{$i}}">{{$i}}</option>
-                            @endfor
+                            @for($i=1;$i<=12;$i++) <option value="{{$i}}">{{$i}}</option>
+                                @endfor
                         </select>
+                        @error('class')
+                        <div>
+                            <label class="error fail-alert  mt-1">{{ $message }}</label>
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="form-group mb-2">
+                        <label for="exampleInputEmail1">{{ __('Description') }}</label>
+                        <textarea class="form-control @error('description') {{ 'is-invalid' }} @enderror" id="description" name="description" rows="3" placeholder="Enter ...">{{@old('name',$data->description)}}</textarea>
+                        @error('description')
+                        <div>
+                            <label class="error fail-alert  mt-1">{{ $message }}</label>
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1" class="">{{ __('Status') }}</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="status" id="active" value="active" @isset($data) @if($data['status']=="active" ) {{"checked"}} @endif @endisset>
+                            <label class="form-check-label" for="active">Active</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="status" id="inactive" value="inactive" @isset($data) @if($data['status']=="inactive" ) {{"checked"}} @endif @endisset>
+                            <label class="form-check-label" for="inactive">Inactive</label>
+                        </div>
+                        @error('status')
+                        <div>
+                            <label class="error fail-alert  mt-1">{{ $message }}</label>
+                        </div>
+                        @enderror
                     </div>
                 </div>
             </div>
