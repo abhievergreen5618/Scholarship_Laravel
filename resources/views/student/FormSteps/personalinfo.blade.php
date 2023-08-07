f4a7926c046f043c91e3dfc0219e205ecbba3f94
shivangi980 <shivangidhiman980@gmail.com>
2023-08-07 11:04:11 +0530

    Update personalinfo.blade.php

--- resources/views/student/FormSteps/personalinfo.blade.php
+++ resources/views/student/FormSteps/personalinfo.blade.php
@@ -98,11 +98,7 @@
                             <option value="">-- Select State --</option>
                             @if(empty($states))
                                 <h3>empty</h3>
-                            <!-- @foreach ($states as $state)
-                            <option value="{{ $state->id }}">
-                                {{ $state->name }}
-                            </option>
-                            @endforeach -->
+                            
                             @endif
                         </select>
 
