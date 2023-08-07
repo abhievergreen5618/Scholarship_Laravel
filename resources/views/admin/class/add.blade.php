50519fc7258b769eee5ea783edae76515cf61919
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-08-07 14:19:44 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -8,56 +8,23 @@
         </div>
         <!-- /.card-header -->
         <!-- form start -->
-        <form id="inspection-type-form" action="" method="POST">
+        <form id="class-add-form" action="" method="POST">
             @csrf
             @isset($data)
             <input type="hidden" name="id" value="{{encrypt($data->id)}}">
             @endisset
             <div class="card-body">
-                <div class="form-group">
-                    <label>Select</label>
-                    <select class="form-control">
-                        <option>Select Class</option>
-                        @for()
-
-                        @endfor
-                    </select>
-                </div>
-                @error('name')
                 <div>
-                    <label class="error fail-alert  mt-1">{{ $message }}</label>
+                    <div class="form-group">
+                        <label>Select</label>
+                        <select class="form-control">
+                            <option>Select Class</option>
+                            @for($i=1;$i<=12:$i++)
+                                {{$i}}
+                            @endfor
+                        </select>
+                    </div>
                 </div>
-                @enderror
-                <div class="form-group mb-2">
-                    <label for="exampleInputEmail1">{{ __('Description') }}</label>
-                    <textarea class="form-control @error('description') {{ 'is-invalid' }} @enderror" id="description" name="description" rows="3" placeholder="Enter ...">{{@old('name',$data->description)}}</textarea>
-                    @error('description')
-                    <div>
-                        <label class="error fail-alert  mt-1">{{ $message }}</label>
-                    </div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1" class="">{{ __('Status') }}</label>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="status" id="active" value="active" @isset($data) @if($data['status']=="active" ) {{"checked"}} @endif @endisset>
-                        <label class="form-check-label" for="active">Active</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="status" id="inactive" value="inactive" @isset($data) @if($data['status']=="inactive" ) {{"checked"}} @endif @endisset>
-                        <label class="form-check-label" for="inactive">Inactive</label>
-                    </div>
-                    @error('status')
-                    <div>
-                        <label class="error fail-alert  mt-1">{{ $message }}</label>
-                    </div>
-                    @enderror
-                </div>
-            </div>
-            <!-- /.card-body -->
-
-            <div class="card-footer">
-                <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
             </div>
         </form>
     </div>
