537c7c5359e500054276c3eca681c0fde3950c8a
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-08-08 14:23:04 +0530

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,10 +35,10 @@
     </div>
     @include('layouts.partials.admin.footer')
     <script src="{{asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-  <script src="{{asset('/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-  <script src="{{asset('/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-  <script src="{{asset('/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('adminlte/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+  <script src="{{asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+  <script src="{{asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+  <script src="{{asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
     <script src="{{asset('js/datashow.js')}}"></script>
 
 
