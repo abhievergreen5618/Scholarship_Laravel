84218b6faad3a47010be96d9799447222a14c014
shivangi980 <shivangidhiman980@gmail.com>
2023-08-21 12:23:32 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/educationinfosubmit','educationInfoStore')->name('educationinfosubmit');
         Route::post('/applicationsummarysubmit','applicationsummarysubmit')->name('applicationsummarysubmit');
         Route::post('/savepaymentdetails','savepaymentdetails')->name('savepaymentdetails');
-        Route::get('/get-districts/{stateCode}','getDistricts');
+        Route::get('/get-districts','getDistricts');
     });
 
 
