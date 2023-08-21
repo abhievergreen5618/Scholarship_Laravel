d004fe0eb688c73e5e37dc69b8be3361dd412e38
shivangi980 <shivangidhiman980@gmail.com>
2023-08-21 14:57:00 +0530

    Update ScholarshipController.php

--- app/Http/Controllers/Student/ScholarshipController.php
+++ app/Http/Controllers/Student/ScholarshipController.php
@@ -40,7 +40,7 @@ class ScholarshipController extends Controller
    public function getDistricts(Request $request)
    {
         $stateCode=$request->post('stateCode');
-        $districts=DistrictModel::where('name',$stateCode)
+        $districts=DB::table('district_models')->where('state_models',$stateCode)
         ->orderBy('name','asc')->get();
         echo $districts;
    }
