8088b2e6f7e223bddac2e0bbd27414346cdbc799
shivangi980 <shivangidhiman980@gmail.com>
2023-08-22 19:38:49 +0530

    Delete toastr.js

--- public/js/toastr.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    toastr.options = {
-        closeButton: true,
-        progressBar: true,
-        positionClass: "toast-top-right",
-        timeOut: 5000 
-    };
-});
