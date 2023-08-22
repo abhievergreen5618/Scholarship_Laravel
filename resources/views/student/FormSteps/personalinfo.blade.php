c2cf1f7dc7a4aa50cc1d24366a6509dcad117916
shivangi980 <shivangidhiman980@gmail.com>
2023-08-22 11:20:50 +0530

    Update personalinfo.blade.php

--- resources/views/student/FormSteps/personalinfo.blade.php
+++ resources/views/student/FormSteps/personalinfo.blade.php
@@ -1,5 +1,5 @@
 <li class="tab-content tab-content-first typography">
-    <form id="frm" action="{{route('personalinfosubmit')}}" enctype="multipart/form-data">
+    <form id="frm" action="{{route('personalinfosubmit')}}" enctype="multipart/form-data" method="POST">
         @csrf
         <input type="hidden" value="{{ encrypt(auth()->user()->id) }}" name="id">
         <div class="tab-content">
