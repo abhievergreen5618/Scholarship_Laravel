11ed0b616349e7fb674e2e93809f42feafbaed90
shivangi980 <shivangidhiman980@gmail.com>
2023-08-23 12:58:57 +0530

    Update bankdetails.blade.php

--- resources/views/student/FormSteps/bankdetails.blade.php
+++ resources/views/student/FormSteps/bankdetails.blade.php
@@ -7,17 +7,20 @@
 </head>
 <body>
     <form>
+    <h3><span>Step [3/6] :</span> Bank Details &nbsp;
+                    <span id="Anthem_ctl00_ContentPlaceHolder1_lblPageMsg__"><span id="ctl00_ContentPlaceHolder1_lblPageMsg" style="color:Red;"></span></span>
+                </h3>
         <table>
     <tr>
-                                <td class="vtext">Account No. <span style="color: red">*</span>
-                                    <br>
-                                    <strong>खाता संख्या</strong>
-                                </td>
-                                <td class="colon">:</td>
-                                <td>
-                                    <span id="Anthem_ctl00_ContentPlaceHolder1_txtAccountNo__"><input name="accountno"  maxlength="15" id="accountno" type="number" class="textboxlong form-control" ondrop="return false;" ondrag="return false;" onpaste="return false;" oncut="return false;" onkeydown="return AllownumberOnly(event,this);"></span>
-                                </td>
-                            </tr>
+        <td class="vtext">Account No. <span style="color: red">*</span>
+        <br>
+            <strong>खाता संख्या</strong>
+        </td>
+        <td class="colon">:</td>
+            <td>
+                <span id="Anthem_ctl00_ContentPlaceHolder1_txtAccountNo__"><input name="accountno"  maxlength="15" id="accountno" type="number" class="textboxlong form-control" ondrop="return false;" ondrag="return false;" onpaste="return false;" oncut="return false;" onkeydown="return AllownumberOnly(event,this);"></span>
+            </td>
+    </tr>
 </table>
 </form>
 </body>
