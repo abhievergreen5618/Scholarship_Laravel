7543ed323b69f9d557524360a234b0b395f18896
shivangi980 <shivangidhiman980@gmail.com>
2023-08-23 13:56:52 +0530

    Update bankdetails.blade.php

--- resources/views/student/FormSteps/bankdetails.blade.php
+++ resources/views/student/FormSteps/bankdetails.blade.php
@@ -1,6 +1,4 @@
-<li class="tab-content tab-content-third typography">
-    
-<input type="hidden" value="{{ encrypt(auth()->user()->id) }}" name="id">
+<li class="tab-content tab-content-2 typography">
 <div class="tab-content">
             <div class="tab-pane active" id="tab_1">
 
