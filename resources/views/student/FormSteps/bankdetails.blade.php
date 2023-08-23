593d8b9deb65672b36450cc8c37a142f05704b2e
shivangi980 <shivangidhiman980@gmail.com>
2023-08-23 16:22:04 +0530

    bank

--- resources/views/student/FormSteps/bankdetails.blade.php
+++ resources/views/student/FormSteps/bankdetails.blade.php
@@ -1,11 +1,10 @@
-<li class="tab-content tab-content-third typography">
-                                <div class="typography">
-    
-<input type="hidden" value="{{ encrypt(auth()->user()->id) }}" name="id">
-<div class="tab-content">
-            <div class="tab-pane active" id="tab_1">
 
-    <form>
+<li class="tab-content tab-content-2 typography">
+    <form id="bankform">
+        @csrf
+        <input type="hidden" value="{{ encrypt(auth()->user()->id) }}" name="id">
+        <div class="tab-content"> 
+            <div class="tab-pane active" id="tab_2">
     <h3><span>Step [3/6] :</span> Bank Details &nbsp;
                     <span><span id="ctl00_ContentPlaceHolder1_lblPageMsg" style="color:Red;"></span></span>
                 </h3>
