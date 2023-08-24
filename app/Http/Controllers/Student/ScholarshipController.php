5e0ee3cf9bb4195cf30b5fb5289d1afe153b439e
shivangi980 <shivangidhiman980@gmail.com>
2023-08-24 11:18:05 +0530

    Update ScholarshipController.php

--- app/Http/Controllers/Student/ScholarshipController.php
+++ app/Http/Controllers/Student/ScholarshipController.php
@@ -240,7 +240,16 @@ class ScholarshipController extends Controller
         }
         else
         {
-            
+            BankDetails::where('id',decrypt($request['id']))->update([
+                "accountno" => $request['accountno'],
+                "cnfrmaccountno" => $request['cnfrmaccountno'],
+                "holdername" => $request['holdername'],
+                "ifsccode" => $request['ifsccode'],
+                "passbook_photo" => $request['passbook_photo'],
+            ]);
+            return response()->json([
+                'message' => 'Saved successfully',
+            ],200);
         }
     }
     
