2cc7452f0130d543790fd32d90a412b971b7d5d6
shivangi980 <shivangidhiman980@gmail.com>
2023-08-24 14:49:32 +0530

    Update formvalidate.js

--- public/js/formvalidate.js
+++ public/js/formvalidate.js
@@ -239,9 +239,9 @@ jQuery('#bankform').validate({
             success: function(result) {
                 if(result.hasOwnProperty("message"))
                 {
-                    $("#tab3").attr('disabled',false);
-                    $("#tab3").trigger('click');
-                    $('[for="tab3"]').find("[data-icon='lock']").remove();
+                    $("#tabb3").attr('disabled',false);
+                    $("#tabb3").trigger('click');
+                    $('[for="tabb3"]').find("[data-icon='lock']").remove();
                     $("#application_summary_step").removeClass("btn-secondary");
                     $("#application_summary_step").addClass("btn-success");
                     debugger;
