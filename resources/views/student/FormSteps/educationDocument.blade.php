b900ca3a25bb831bc89bd0432be5f4d5a5370ac6
shivangi980 <shivangidhiman980@gmail.com>
2023-08-25 16:23:18 +0530

    Update educationDocument.blade.php

--- resources/views/student/FormSteps/educationDocument.blade.php
+++ resources/views/student/FormSteps/educationDocument.blade.php
@@ -145,7 +145,7 @@
                                                 <span id="Anthem_ctl00_ContentPlaceHolder1_gvsubject_ctl02_txtCreMarkPercent__"><input name="class_percentage" type="number" maxlength="5" id="class_percentage" class="textbox form-control" onpaste="return false" ondrop="return false" onkeydown="return NumberOnly(event,this);" style="width:55px;" value="{{isset($step2schooldata['percentage_marks']) ? $step2schooldata['percentage_marks'] : '' }}"></span>
 
                                             </td>
-                                            <td align="center" style="width:10%;>
+                                            <td align="center" style="width:8%;>
                                                 <span id="Anthem_ctl00_ContentPlaceHolder1_gvsubject_ctl02_txtExamRollNo__"><input name="class_rollno" type="number" maxlength="15" id="class_rollno" class="textbox form-control" onpaste="return false" ondrop="return false"  value="{{isset($step2schooldata['exam_roll_no']) ? $step2schooldata['exam_roll_no'] : '' }}"></span>
                                             </td>
 
