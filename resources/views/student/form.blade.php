f406ac4253360b139d168197b1d52f395a728d0a
shivangi980 <shivangidhiman980@gmail.com>
2023-08-28 11:24:04 +0530

    Update form.blade.php

--- resources/views/student/form.blade.php
+++ resources/views/student/form.blade.php
@@ -117,10 +117,61 @@
                                                         </tr>
 
                                                         <tr>
+                                                            <td colspan="3"></td>
+                                                        </tr>
+                                                        <tr>
+                                                        <td width="42%" class="vtext"><strong>Application Number</strong>
+                                                            </td>
+                                                            <td class="colon">:</td>
                                                             <td>
-                                                                <button type="submit">Download Reciept</button>
+                                                                <span
+                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
+                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step5_updated_at))
+                                                                            ? auth()->user()->application_number :
+                                                                            ''}}</b></span></span>
+                                                            </td>
+                                                            <td width="42%" class="vtext"><strong>Transaction Number</strong>
+                                                            </td>
+                                                            <td class="colon">:</td>
+                                                            <td>
+                                                                <span
+                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
+                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step5_updated_at))
+                                                                            ? auth()->user()->transation_number :
+                                                                            ''}}</b></span></span>
+                                                            </td>
+                                                            <td width="42%" class="vtext"><strong>Name</strong>
+                                                            </td>
+                                                            <td class="colon">:</td>
+                                                            <td>
+                                                                <span
+                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
+                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step1_updated_at))
+                                                                            ? auth()->user()->name :
+                                                                            ''}}</b></span></span>
+                                                            </td>
+                                                            <td width="42%" class="vtext"><strong>Mobile No.</strong>
+                                                            </td>
+                                                            <td class="colon">:</td>
+                                                            <td>
+                                                                <span
+                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
+                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step1_updated_at))
+                                                                            ? auth()->user()->mobleno :
+                                                                            ''}}</b></span></span>
+                                                            </td>
+                                                            <td width="42%" class="vtext"><strong>Email-ID</strong>
+                                                            </td>
+                                                            <td class="colon">:</td>
+                                                            <td>
+                                                                <span
+                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
+                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step1_updated_at))
+                                                                            ? auth()->user()->email :
+                                                                            ''}}</b></span></span>
                                                             </td>
                                                         </tr>
+
                                                     </tbody>
                                                 </table>
                                             </div>
