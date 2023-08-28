e2ac22a6a57930bf785256eec54aacd5755b41af
shivangi980 <shivangidhiman980@gmail.com>
2023-08-28 19:30:15 +0530

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -43,7 +43,7 @@ return new class extends Migration
             $table->string('signature')->nullable();
             $table->string('reference_number')->nullable();
             $table->string('roll_number')->nullable();
-            $table->string('apllication_number')->nullable();
+            $table->string('application_number')->nullable();
             $table->string('transaction_id')->nullable();
             $table->timestamp('step1_updated_at')->nullable();
             $table->timestamp('step2_updated_at')->nullable();
