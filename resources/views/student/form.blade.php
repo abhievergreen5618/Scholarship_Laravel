daf5dfcc53741fb8f4e274ce0a084fe742bcbd2e
shivangi980 <shivangidhiman980@gmail.com>
2023-08-28 18:52:59 +0530

    Update form.blade.php

--- resources/views/student/form.blade.php
+++ resources/views/student/form.blade.php
@@ -48,11 +48,11 @@
                         </label>
 
                         <input type="radio" name="pcss3t" id="tab5" class="tab-content-last"
-                            {{!empty(auth()->user()->step4_updated_at) && empty(auth()->user()->step4_updated_at) ?
+                            {{!empty(auth()->user()->step4_updated_at) && empty(auth()->user()->step5_updated_at) ?
                         'checked' : ''}} {{empty(auth()->user()->step4_updated_at) ||
                         !empty(auth()->user()->step5_updated_at) ? 'disabled' : ''}}>
                         <label for="tab5"><i class="fas fa-hand-point-right"></i>Payment
-                            @if(empty(auth()->user()->step3_updated_at) || !empty(auth()->user()->step4_updated_at)) <i
+                            @if(empty(auth()->user()->step4_updated_at) || !empty(auth()->user()->step5_updated_at)) <i
                                 class="fa fa-lock" aria-hidden="true"></i> @endif
                         </label>
 
