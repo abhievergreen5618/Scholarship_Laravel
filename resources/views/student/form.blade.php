ff26b691a99717ae41ce0833891cd98df3381915
shivangi980 <shivangidhiman980@gmail.com>
2023-08-29 16:42:19 +0530

    pdf

--- resources/views/student/form.blade.php
+++ resources/views/student/form.blade.php
@@ -122,76 +122,8 @@
                                                         <tr>
                                                             <td colspan="3"></td>
                                                         </tr>
-                                                    <form>
-                                                        <tr>
-                                                            <td colspan="3"></td>
-                                                        </tr>
-                                                        <tr>
-                                                        <td width="42%" class="vtext"><strong>Application Number</strong>
-                                                            </td>
-                                                            <td class="colon">:</td>
-                                                            <td>
-                                                                <span
-                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
-                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step5_updated_at))
-                                                                            ? auth()->user()->application_number :
-                                                                            ''}}</b></span></span>
-                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                            <td width="42%" class="vtext"><strong>Transaction ID</strong>
-                                                            </td>
-                                                            <td class="colon">:</td>
-                                                            <td>
-                                                                <span
-                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
-                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step5_updated_at))
-                                                                            ? auth()->user()->transaction_id :
-                                                                            ''}}</b></span></span>
-                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                            <td width="42%" class="vtext"><strong>Name</strong>
-                                                            </td>
-                                                            <td class="colon">:</td>
-                                                            <td>
-                                                                <span
-                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
-                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step5_updated_at))
-                                                                            ? auth()->user()->name :
-                                                                            ''}}</b></span></span>
-                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                            <td width="42%" class="vtext"><strong>Mobile No.</strong>
-                                                            </td>
-                                                            <td class="colon">:</td>
-                                                            <td>
-                                                                <span
-                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
-                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step5_updated_at))
-                                                                            ? auth()->user()->mobileno :
-                                                                            ''}}</b></span></span>
-                                                            </td>
-                                                                        </tr>
-                                                                        <tr>
-                                                            <td width="42%" class="vtext"><strong>Email-ID</strong>
-                                                            </td>
-                                                            <td class="colon">:</td>
-                                                            <td>
-                                                                <span
-                                                                    id="Anthem_ctl00_ContentPlaceHolder1_lblCoursName__"><span
-                                                                        id="ctl00_ContentPlaceHolder1_lblCoursName"><b>{{(!empty(auth()->user()->step5_updated_at))
-                                                                            ? auth()->user()->email :
-                                                                            ''}}</b></span></span>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td colspan="3">
-                                                            <a class="btn btn-primary" href="downloadpdf">Download Receipt</a>
-                                                            </td>
-                                                        </tr>
-                                                    </form>                
+                                                           
+                                                        @include("student.FormSteps.pdf")
                                                     </tbody>
                                                 </table>
                                             </div>
