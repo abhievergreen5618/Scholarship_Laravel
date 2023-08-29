f577189d204bbc30291047be63fdde1806325858
shivangi980 <shivangidhiman980@gmail.com>
2023-08-29 16:23:22 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::post('districtslist',[ScholarshipController::class,'getDistricts']);
 
-    Route::resource('downloadPdf',[ScholarshipController::class,'downloadpdf']);
+    Route::get('downloadPdf',[ScholarshipController::class,'downloadpdf']);
 
     // Admin Routes
     Route::controller(DashboardController::class)->group(function () {
