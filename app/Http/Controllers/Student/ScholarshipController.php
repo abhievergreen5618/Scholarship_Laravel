aad998c19e5f3160f71b413e839e6fff2f95d0b7
shivangi980 <shivangidhiman980@gmail.com>
2023-08-29 17:32:39 +0530

    Update ScholarshipController.php

--- app/Http/Controllers/Student/ScholarshipController.php
+++ app/Http/Controllers/Student/ScholarshipController.php
@@ -314,7 +314,7 @@ class ScholarshipController extends Controller
 	    if($request->has('download'))
 	    {
 	        $pdf = PDF::loadView('student.FormSteps.pdffile',$data);
-	        return $pdf->download('student.FormSteps.pdffile.pdf');
+	        return $pdf->download('pdffile.pdf');
 	    }
         else{
             return "Download parameter not provided.";
