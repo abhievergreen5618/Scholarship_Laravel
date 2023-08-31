c184a56153653ca975e90ca1df2e2680290e33e5
shivangi980 <shivangidhiman980@gmail.com>
2023-08-31 15:14:11 +0530

    subject

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,6 +38,7 @@ class User extends Authenticatable
         'singlegirlchild',
         'applyingfor',
         'applyingforsubject',
+        'subject',
         'physicallychallenged',
         'category',
         'physicallychallengedproof',
