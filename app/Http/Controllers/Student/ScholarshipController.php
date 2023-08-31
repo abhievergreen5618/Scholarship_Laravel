222ea3935dfbfe01295ed704967811f27192e093
shivangi980 <shivangidhiman980@gmail.com>
2023-08-31 16:54:11 +0530

    Update ScholarshipController.php

--- app/Http/Controllers/Student/ScholarshipController.php
+++ app/Http/Controllers/Student/ScholarshipController.php
@@ -23,7 +23,7 @@ class ScholarshipController extends Controller
     {
         $states = StateModel::orderBy('name','asc')->orderBy('code','asc')->get();
         $subjects = Subject::orderBy('name','asc')->get();
-        // dd($subjects);
+        dd($subjects);
         if(!empty(Auth::user()->step2_updated_at))
         {
             $step2schooldata = EducationDetails::where(['user_id' =>Auth::user()->id,'type' => 'school'])->first();
