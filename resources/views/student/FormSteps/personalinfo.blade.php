c459ec25eb813de89f96aead7619fadc037099af
shivangi980 <shivangidhiman980@gmail.com>
2023-08-31 16:28:23 +0530

    Update personalinfo.blade.php

--- resources/views/student/FormSteps/personalinfo.blade.php
+++ resources/views/student/FormSteps/personalinfo.blade.php
@@ -397,7 +397,7 @@
                                         <select name="subject" id="applyingforsubject"
                                             class="dropdownlong form-control">
                                             <option value="">--Please Select--</option>
-                                            @if(!empty($subjects))
+                                            @if(!$subjects->isEmpty())
                                             @foreach($subjects as $subject)
                                             <option value="{{ $subject->id }}">
                                                 {{ $subject->name }}
