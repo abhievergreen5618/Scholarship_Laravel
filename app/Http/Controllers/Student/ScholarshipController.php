85cb5c0e93637685a0667d11cdf14204c00abeb2
shivangi980 <shivangidhiman980@gmail.com>
2023-08-31 16:01:08 +0530

    Update ScholarshipController.php

--- app/Http/Controllers/Student/ScholarshipController.php
+++ app/Http/Controllers/Student/ScholarshipController.php
@@ -32,12 +32,12 @@ class ScholarshipController extends Controller
             return view('student.form')->with([
                 "step2schooldata" => $step2schooldata,
                 "states" => $states,
-                "subject" => $subject
+                "subject" => $subjects
             ]);
         }
         else
         {
-            return view('student.form')->with(['states'=>$states],['subject'=>$subject]);
+            return view('student.form')->with(['states'=>$states],['subject'=>$subjects]);
         }
     }
 
