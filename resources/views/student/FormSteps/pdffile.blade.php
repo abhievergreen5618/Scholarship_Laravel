ad49452f7405979e0eb573f036b351ecc0c6d3ee
shivangi980 <shivangidhiman980@gmail.com>
2023-08-31 10:47:38 +0530

    Update pdffile.blade.php

--- resources/views/student/FormSteps/pdffile.blade.php
+++ resources/views/student/FormSteps/pdffile.blade.php
@@ -64,7 +64,7 @@
                                                         </tr>
                                                         <tr>
                                                             <td colspan="3">
-                                                            <a class="btn" href="{{ route('downloadPdf', ['download' => 'pdf']) }}">Download Receipt</a>
+                                                            <a class="btn" href="{{ route('downloadPdf', ['download' => true]) }}">Download Receipt</a>
                                                         </td>
                                                         </tr>
                                                     </form>         
