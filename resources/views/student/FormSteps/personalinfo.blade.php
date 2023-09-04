4aa1512f03220aa34e54e527f84ae5550a8c0d18
shivangi980 <shivangidhiman980@gmail.com>
2023-09-04 14:14:22 +0530

    Update personalinfo.blade.php

--- resources/views/student/FormSteps/personalinfo.blade.php
+++ resources/views/student/FormSteps/personalinfo.blade.php
@@ -341,7 +341,7 @@
                                 <td>
                                     <span id="Anthem_ctl00_ContentPlaceHolder1_ddlSportCulturalBoth__">
                                         <select name="subjects" id="subjects"
-                                            class="dropdownlong form-control" data-live-search="true" multiple>
+                                            class="dropdownlong form-control" data-live-search="true" >
                                             <option value="" >--Please Select--</option>
                                             @foreach($subjects as $subject)
                                             <option value="{{ $subject->name }}">
