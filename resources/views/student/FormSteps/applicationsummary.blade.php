0a9683c1de7167d806f70a18c8ed6ba2b8b4410d
shivangi980 <shivangidhiman980@gmail.com>
2023-09-04 12:53:42 +0530

    Update applicationsummary.blade.php

--- resources/views/student/FormSteps/applicationsummary.blade.php
+++ resources/views/student/FormSteps/applicationsummary.blade.php
@@ -1,7 +1,7 @@
-<li class="tab-content tab-content-fourth typography"> 
+<li class="tab-content tab-content-last typography"> 
 <div class="typography">
     <div class="tab-content">
-        <div class="tab-pane active" id="tab_4" >
+        <div class="tab-pane active" id="tab_5" >
             <h3><span>Step [4/6] :</span> Application Summary &nbsp;
                 <span id="Anthem_ctl00_ContentPlaceHolder1_lblPageMsg__"><span id="ctl00_ContentPlaceHolder1_lblPageMsg"
                         style="color:Red;"></span></span>
