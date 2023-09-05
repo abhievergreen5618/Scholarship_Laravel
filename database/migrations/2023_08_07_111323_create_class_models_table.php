21f89db151d73c5545fd848d2c9ea4f856a3fbff
shivangi980 <shivangidhiman980@gmail.com>
2023-09-05 15:17:35 +0530

    Update 2023_08_07_111323_create_class_models_table.php

--- database/migrations/2023_08_07_111323_create_class_models_table.php
+++ database/migrations/2023_08_07_111323_create_class_models_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string("class");
             $table->string("description");
             $table->string("status")->default("active");
-            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
             $table->timestamps();
         });
     }
