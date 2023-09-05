3f5e2992a215a974635717693282c633a1da01e0
shivangi980 <shivangidhiman980@gmail.com>
2023-09-05 19:18:17 +0530

    delete

--- app/Http/Controllers/Admin/ClassController.php
+++ app/Http/Controllers/Admin/ClassController.php
@@ -28,7 +28,8 @@ class ClassController extends Controller
                 ->addColumn('action', function ($row) {
                     $id = encrypt($row->id);
                     $editlink = route('admin.class.edit', ['id' => $id]);
-                    $btn = "<div class='d-flex justify-content-around'><a href='$editlink' data-id='$id' data-bs-toggle='tooltip' data-bs-placement='top' title='Edit' class='btn limegreen btn-primary  edit'><i class='fas fa-edit'></i></a><a href='javascript:void(0)' data-id='$id' class='delete btn red-btn btn-danger  '  data-bs-toggle='tooltip' data-bs-placement='top' title='Delete'><i class='fa fa-trash' aria-hidden='true'></i></a></div>";
+                    $deletelink = route('admin.class.delete',['id' => $id]);
+                    $btn = "<div class='d-flex justify-content-around'><a href='$editlink' data-id='$id' data-bs-toggle='tooltip' data-bs-placement='top' title='Edit' class='btn limegreen btn-primary  edit'><i class='fas fa-edit'></i></a><a href='$deletelink' data-id='$id' class='delete btn red-btn btn-danger  '  data-bs-toggle='tooltip' data-bs-placement='top' title='Delete'><i class='fa fa-trash' aria-hidden='true'></i></a></div>";
                     return $btn;
                 })
                 ->rawColumns(['action'])
