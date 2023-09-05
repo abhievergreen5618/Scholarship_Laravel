285af64e1767567288a79f07105c48164f02e934
shivangi980 <shivangidhiman980@gmail.com>
2023-09-05 17:57:17 +0530

    Update ClassModel.php

--- app/Models/ClassModel.php
+++ app/Models/ClassModel.php
@@ -8,10 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ClassModel extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
-        "name",
-        "classes",
+        "class",
         "description",
         "status",  
     ];
