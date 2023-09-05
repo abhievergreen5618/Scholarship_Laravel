dbcc8df38d58b100822781b18d4952896e75b33a
shivangi980 <shivangidhiman980@gmail.com>
2023-09-05 18:48:02 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -8,7 +8,7 @@
         </div>
         <!-- /.card-header -->
         <!-- form start -->
-        <form id="class-add-form" action="{{isset($data) ? route('admin.class.update') : route('admin.class.store') : route('admin.class.delete')}}" method="POST">
+        <form id="class-add-form" action="{{isset($data) ? route('admin.class.update') : route('admin.class.store') }}" method="POST">
             @csrf
             @isset($data)
             <input type="hidden" name="id" value="{{encrypt($data->id)}}">
