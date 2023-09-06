0e52703ffdf6eea0e8ccff15db8a7f982f6517d5
shivangi980 <shivangidhiman980@gmail.com>
2023-09-06 14:33:54 +0530

    Update SubjectController.php

--- app/Http/Controllers/Admin/SubjectController.php
+++ app/Http/Controllers/Admin/SubjectController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\SubjectRequest;
 use Illuminate\Http\Request;
 use App\Models\Subject;
+use App\Models\ClassModel;
 use Yajra\DataTables\Facades\DataTables;
 
 class SubjectController extends Controller
