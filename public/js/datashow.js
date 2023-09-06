49993ab1da306b82975a3f82fc6aa4a46dd09ea2
shivangi980 <shivangidhiman980@gmail.com>
2023-09-06 11:36:03 +0530

    Update datashow.js

--- public/js/datashow.js
+++ public/js/datashow.js
@@ -99,7 +99,7 @@ $(document).ready(function () {
     });
 });
 
-classtable.on('click','delete',function(){
+classtable.on('click','.delete',function(){
     $('#classtable_processing').show();
     element = $(this);
     var classsid = $(this).attr('data-id');
