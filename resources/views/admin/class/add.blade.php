930d993de9b83c95a1fc2402676535b66bc26be6
shivangi980 <shivangidhiman980@gmail.com>
2023-09-07 23:31:23 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -19,17 +19,11 @@
                     <div class="form-group">
                         <label>Class</label>
                         <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="class">
-    <option value="">Select Class</option>
-    @for($i=1; $i<=12; $i++) 
-        @php
-            $existingClass = ClassModel::where('class', $i)->first();
-        @endphp
-        @if (!$existingClass)
-            <option value="{{$i}}" @isset($data) @if($data['class'] == $i ) {{"selected"}} @endif @endisset>{{$i}}</option>
-        @endif
-    @endfor
-</select>
-
+                            <option value="">Select Class</option>
+                            @for($i=1;$i<=12;$i++) 
+                                <option value="{{$i}}" @isset($data) @if($data['class']== $i ) {{"selected"}} @endif @endisset>{{$i}}</option>
+                            @endfor
+                        </select>
                         @error('class')
                         <div>
                             <label class="error fail-alert  mt-1">{{ $message }}</label>
