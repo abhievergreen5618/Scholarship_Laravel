83e78fe33460d6eef57299add8c761989bd91d01
shivangi980 <shivangidhiman980@gmail.com>
2023-09-07 11:44:15 +0530

    Update pdffile.blade.php

--- resources/views/student/FormSteps/pdffile.blade.php
+++ resources/views/student/FormSteps/pdffile.blade.php
@@ -1,11 +1,4 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<!-- 
 
 <body class="border_design">
 
@@ -78,6 +71,5 @@
 	</div>
 </div>
 </div>
-</body>
 
-</html> -->
+ -->
