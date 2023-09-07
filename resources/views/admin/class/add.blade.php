b3f3efb96c77d40cf9294bc8c0663d7b19a9bb28
shivangi980 <shivangidhiman980@gmail.com>
2023-09-07 22:28:58 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -8,39 +8,51 @@
         </div>
         <!-- /.card-header -->
         <!-- form start -->
-        <form id="class-add-form" action="{{isset($data) ? route('admin.class.update') : route('admin.class.store') }}" method="POST">
+        <form id="class-add-form" action="{{isset($data) ? route('admin.class.update') : route('admin.class.store') }}"
+            method="POST">
             @csrf
-            
+
             @isset($data)
             <input type="hidden" name="id" value="{{encrypt($data->id)}}">
-            @endisset 
+            @endisset
             <div class="card-body">
                 <div>
                     <div class="form-group">
                         <label>Class</label>
                         <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="class">
                             <option value="">Select Class</option>
-                            @for($i=1;$i<=12;$i++) <option value="{{$i}}" @isset($data) @if($data['class']== $i ) {{"selected"}} @endif @endisset>{{$i}}</option>
+                            @for($i=1;$i<=12;$i++)
+                                @php $isSelected=isset($data) && $data['class']==$i;
+                                @endphp 
+                                    @if(!$isSelected) 
+                                    <option value="{{ $i }}" {{ $isSelected ? 'selected' : '' }}>{{ $i }}
+                                </option>
+                                @endif
                                 @endfor
                         </select>
+
                         @error('class')
                         <div>
                             <label class="error fail-alert  mt-1">{{ $message }}</label>
-                        </div> 
+                        </div>
                         @enderror
                     </div>
                     <div class="form-group mb-2">
                         <label for="exampleInputEmail1">{{ __('Description') }}</label>
-                        <textarea class="form-control @error('description') {{ 'is-invalid' }} @enderror" id="description" name="description" rows="3" placeholder="Enter ...">{{@old('name',$data->description)}}</textarea>
+                        <textarea class="form-control @error('description') {{ 'is-invalid' }} @enderror"
+                            id="description" name="description" rows="3"
+                            placeholder="Enter ...">{{@old('name',$data->description)}}</textarea>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1" class="">{{ __('Status') }}</label>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="status" id="active" value="active" @isset($data) @if($data['status']=="active" ) {{"checked"}} @endif @endisset>
+                            <input class="form-check-input" type="radio" name="status" id="active" value="active"
+                                @isset($data) @if($data['status']=="active" ) {{"checked"}} @endif @endisset>
                             <label class="form-check-label" for="active">Active</label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="status" id="inactive" value="inactive" @isset($data) @if($data['status']=="inactive" ) {{"checked"}} @endif @endisset>
+                            <input class="form-check-input" type="radio" name="status" id="inactive" value="inactive"
+                                @isset($data) @if($data['status']=="inactive" ) {{"checked"}} @endif @endisset>
                             <label class="form-check-label" for="inactive">Inactive</label>
                         </div>
                         @error('status')
