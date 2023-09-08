caebeae2591c56b0370c22235b43aee437c6d760
shivangi980 <shivangidhiman980@gmail.com>
2023-09-08 18:36:12 +0530

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -47,6 +47,7 @@
   <script src="{{asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
   <script src="{{asset('adminlte/plugins/select2/js/select2.full.min.js')}}"></script>
     <script src="{{asset('/js/sweetalert.min.js')}}"></script>
+    <script src="{{asset('/js/validate.min.js')}}"></script>
     <script src="{{asset('js/datashow.js')}}"></script>
 
 
