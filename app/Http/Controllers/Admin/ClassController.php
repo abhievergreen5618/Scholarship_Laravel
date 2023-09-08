90f53897499b2a57689c0e3e1c34b3e0d1e77ca7
shivangi980 <shivangidhiman980@gmail.com>
2023-09-08 16:45:06 +0530

    Update ClassController.php

--- app/Http/Controllers/Admin/ClassController.php
+++ app/Http/Controllers/Admin/ClassController.php
@@ -49,9 +49,8 @@ class ClassController extends Controller
                     return $statusBtn;
                 })
                 ->addColumn('subject_id',function($row){
-                $class = 6;
-                $classInstance = ClassModel::find($class);
-                dd($classInstance);
+                
+                $classInstance = ClassModel::find('class');
                 if ($classInstance) {
                     $subjectName = $classInstance->subject->name;
                     return $subjectName;
