09538287f3db9a8b5a89dca663e01a29760e7acf
shivangi980 <shivangidhiman980@gmail.com>
2023-09-08 18:50:07 +0530

    Update add.blade.php

--- resources/views/admin/subject/add.blade.php
+++ resources/views/admin/subject/add.blade.php
@@ -58,6 +58,8 @@
                             <input class="form-check-input" type="radio" name="status" id="inactive" value="inactive" @isset($data) @if($data['status']=="inactive" ) {{"checked"}} @endif @endisset>
                             <label class="form-check-label" for="inactive">Inactive</label>
                         </div>
+
+                        
                         @error('status')
                         <div>
                             <label class="error fail-alert  mt-1">{{ $message }}</label>
