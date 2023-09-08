075f87e3e2967fcd006273bdd570adbaa75776a0
shivangi980 <shivangidhiman980@gmail.com>
2023-09-08 12:00:19 +0530

    css

--- resources/views/admin/subject/index.blade.php
+++ resources/views/admin/subject/index.blade.php
@@ -29,7 +29,7 @@
                         <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
                             <div class="row">
                                 <div class="col-sm-12">
-                                    <table id="subjecttable" class="table table-bordered table-striped">
+                                    <table style="text-align:center;" id="subjecttable" class="table table-bordered table-striped">
                                         <thead>
                                             <tr>
                                                 <th>Subject Name</th>
