4d8fbee886a4e3cf7fc199add6695d8d8152df62
shivangi980 <shivangidhiman980@gmail.com>
2023-09-09 10:21:08 +0530

    env

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.master')
+
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Subjects</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Subjects</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Details</h3>
+                    </div>
+                    <div class="card-body">
+                        <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <table style="text-align:center;" id="usertable" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Email-ID</th>
+                                                <th>Mobile No.</th>
+                                                <th>Class</th>
+                                                <th>Gender</th>
+                                                <th>DOB</th>
+                                                <th>Address</th>
+                                                <th>Status</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+@endsection
--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -120,13 +120,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('admin.scholarshiptype.add')}}" class="nav-link active">
+                <a href="{{route('admin.user.add')}}" class="nav-link active">
                   <i class="fas fa-plus  nav-icon"></i>
                   <p>Add User</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{route('admin.scholarshiptype.index')}}" class="nav-link">
+                <a href="{{route('admin.user.index')}}" class="nav-link">
                   <i class="fas fa-list nav-icon"></i>
                   <p>View All User</p>
                 </a>
