4ae842508a714df0a78d61e470485e32885c4608
shivangi980 <shivangidhiman980@gmail.com>
2023-09-09 14:40:59 +0530

    Update index.blade.php

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -5,12 +5,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Subjects</h1>
+                <h1>Users</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Subjects</li>
+                    <li class="breadcrumb-item active">Users</li>
                 </ol>
             </div>
         </div>
