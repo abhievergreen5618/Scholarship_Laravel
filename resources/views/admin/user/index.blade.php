3e17aebba78b5245f41c0496a485d0b11515dfb1
shivangi980 <shivangidhiman980@gmail.com>
2023-09-09 16:08:42 +0530

    Update index.blade.php

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -17,7 +17,7 @@
     </div>
 </section>
 
-<div class="card-header">
+<div class="card-header" style="background-color:#007bff; color:white;">
       <h3 class="card-title">Filter</h3>
       <div class="card-tools">
         <button type="button" class="btn btn-tool" data-card-widget="collapse">
