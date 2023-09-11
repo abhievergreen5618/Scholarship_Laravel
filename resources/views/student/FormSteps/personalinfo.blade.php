b4f664be5bb95bf6fd1922c828115bc56bf08907
shivangi980 <shivangidhiman980@gmail.com>
2023-09-11 18:32:52 +0530

    Update personalinfo.blade.php

--- resources/views/student/FormSteps/personalinfo.blade.php
+++ resources/views/student/FormSteps/personalinfo.blade.php
@@ -486,7 +486,9 @@
                                 <td class="colon">:</td><span style="color: red">*</span>
                                 <td>
         <div id="fee" {{ !empty(auth()->user()->physicallychallenged == "yes") ? 'style="display:none;"' : ''}} >
-            {{ $fee }}
+        @foreach($fee as $feeDetail)
+        {{ $feeDetail['fee'] }}
+    @endforeach
         </div>
    
 
