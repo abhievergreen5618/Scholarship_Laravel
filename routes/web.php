0259e7be6e3cd5f4b5b2cff05e415908aa542035
shivangi980 <shivangidhiman980@gmail.com>
2023-09-11 19:32:42 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::controller(ClassController::class)->group(function () {
         Route::get('/classlist','index')->name('admin.class.index'); 
-        Route::get('/addclass','create')->name('admin.class.add'); 
+        Route::get('/addclass','create')->name('admin.classes'); 
         Route::post('/createclass','store')->name('admin.class.store');
         Route::post('/classdetails', 'display')->name('admin.class.details');
         Route::get('/classedit/{id}', 'edit')->name('admin.class.edit');
