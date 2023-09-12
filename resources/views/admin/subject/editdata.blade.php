dce06b14d9af695d5291413a5cbfaa3b39b6708f
shivangi980 <shivangidhiman980@gmail.com>
2023-09-12 18:11:37 +0530

    Update editdata.blade.php

--- resources/views/admin/subject/editdata.blade.php
+++ resources/views/admin/subject/editdata.blade.php
@@ -28,13 +28,13 @@
                         <label>Class</label>
                         <div class="select2-purple">
                         <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="classes[]" id="class" multiple="multiple" data-placeholder="Select Classes" data-dropdown-css-class="select2-purple" >
-    <option value="">Select Classes</option>
-    @if (!empty($classSelect))
-        @foreach (json_decode($classSelect) as $class)
-            <option value="{{ $class }}">{{ $class }}</option>
-        @endforeach
-    @endif
-</select>
+                            <option value="">Select Classes</option>
+                            @if (!empty($classSelect))
+                                @foreach (json_decode($classSelect) as $class)
+                                    <option value="{{ $class }}">{{ $class }}</option>
+                                @endforeach
+                            @endif
+                        </select>
 
                         </div>
                         @error('class')
