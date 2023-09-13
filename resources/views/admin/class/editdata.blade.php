412cd328c22c7f262340292521a1d7b95a00b3d0
shivangi980 <shivangidhiman980@gmail.com>
2023-09-13 12:51:42 +0530

    Update editdata.blade.php

--- resources/views/admin/class/editdata.blade.php
+++ resources/views/admin/class/editdata.blade.php
@@ -19,18 +19,14 @@
                     <div class="form-group">
                         <label>Class</label>
 
-                        <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="class" id="class" data-placeholder="Select Class" data-dropdown-css-class="select2-purple" @if (isset($data)) disabled @endif>
-    <option value="">Select Class</option>
-    @for ($i = 1; $i <= 12; $i++)
-        @php
-            $selected = (!isset($data) || $data['class'] != $i) ? '' : 'selected';
-            $valueExists = DB::table('class_models')->where('class', $i)->exists();
-        @endphp
-        <option value="{{ $i }}" {{ $selected }} @if ($valueExists) disabled @endif>
-            {{ $i }}
-        </option>
-    @endfor
-</select>
+                        @php
+    $selectedId = (!isset($data) || empty($data['class'])) ? '' : implode(', ', $data['class']);
+    $valueExists = DB::table('class_models')->whereIn('class', $data['class'])->exists();
+@endphp
+
+<input type="text" class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="class" value="{{ $selectedId ?: 'Select Value' }}" readonly disabled>
+
+
 
 
 
