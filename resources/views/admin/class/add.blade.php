c72ae74073050f9c4be3d3d2d0fd06614a7fdf0c
shivangi980 <shivangidhiman980@gmail.com>
2023-09-13 11:01:31 +0530

    Update add.blade.php

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -18,7 +18,7 @@
                 <div>
                     <div class="form-group">
                         <label>Class</label>
-                        <!-- <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="class">
+                        <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="class">
     <option value="">Select Class</option>
     @for($i=1;$i<=12;$i++)
         @php
@@ -31,21 +31,7 @@
         }
         @endphp
     @endfor
-</select> -->
-
-<select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="class" id="class" data-placeholder="Select Class" data-dropdown-css-class="select2-purple">
-    <option value="">Select Class</option>
-    @for ($i = 1; $i <= 12; $i++)
-        @php
-            $selected = (!isset($data) || $data['class'] != $i) ? '' : 'selected';
-            $valueExists = DB::table('class_models')->where('class', $i)->exists();
-        @endphp
-        <option value="{{ $i }}" {{ $selected }} @if ($valueExists) disabled @endif>
-            {{ $i }}
-        </option>
-    @endfor
 </select>
-
 
 
 
