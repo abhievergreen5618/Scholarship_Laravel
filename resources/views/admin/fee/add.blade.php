312b6066564ad88a6545003fd4bf9ff777aec462
shivangi980 <shivangidhiman980@gmail.com>
2023-09-14 14:05:52 +0530

    Update add.blade.php

--- resources/views/admin/fee/add.blade.php
+++ resources/views/admin/fee/add.blade.php
@@ -34,11 +34,12 @@
 
     <option value="General" @if(!empty(auth()->user()->step1_updated_at))
 {{auth()->user()->category == "General" ? 'selected' : ''}} @endif>General</option> -->
-
+@if(empty($classes))
+vbn
 @foreach($classes as $class)
     <option value="{{ $class->class }}">{{ $class->class }}</option>
 @endforeach
-
+@endif
 </select>
 
                         @error('feetype')
