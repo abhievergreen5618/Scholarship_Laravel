cb49e5483fbe0364eaf056d1bfabab6223f2a22b
shivangi980 <shivangidhiman980@gmail.com>
2023-09-14 14:04:53 +0530

    Update ClassController.php

--- app/Http/Controllers/Admin/ClassController.php
+++ app/Http/Controllers/Admin/ClassController.php
@@ -63,7 +63,6 @@ class ClassController extends Controller
     public function create()
     {
         $classes = ClassModel::orderBy('class','asc')->get();
-        dd($classes);
         return view("admin.class.add")->with('classes',$classes);
     }
 
