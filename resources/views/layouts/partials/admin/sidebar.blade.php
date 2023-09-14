e20a19a468d67109b4cb853bf9101946b2eb63a2
shivangi980 <shivangidhiman980@gmail.com>
2023-09-14 14:19:54 +0530

    feeseeder

--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -143,12 +143,12 @@
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
+              <!-- <li class="nav-item">
                 <a href="{{route('admin.fee.add')}}" class="nav-link active">
                   <i class="fas fa-plus  nav-icon"></i>
                   <p>Add Fee</p>
                 </a>
-              </li>
+              </li> -->
               <li class="nav-item">
                 <a href="{{route('admin.fee.index')}}" class="nav-link">
                   <i class="fas fa-list nav-icon"></i>
