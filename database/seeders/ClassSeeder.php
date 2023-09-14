23196398ceef88dff4b090094d6708e337b6141d
shivangi980 <shivangidhiman980@gmail.com>
2023-09-14 12:26:02 +0530

    classseeders

---
+++ database/seeders/ClassSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClassSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $classes = [
+            ['class' => '1'],
+            ['class' => '2'],
+            ['class' => '3'],
+            ['class' => '4'],
+            ['class' => '5'],
+            ['class' => '6'],
+            ['class' => '7'],
+            ['class' => '8'],
+            ['class' => '9'],
+            ['class' => '10'],
+            ['class' => '11'],
+            ['class' => '12'],
+        ];
+
+        ClassModel::insert($classes);
+    }
+}
