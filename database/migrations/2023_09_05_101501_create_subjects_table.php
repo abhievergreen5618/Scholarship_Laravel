612436e74db3e118801df99ee4cfa3e01f6ed456
shivangi980 <shivangidhiman980@gmail.com>
2023-09-14 18:39:36 +0530

    Update 2023_09_05_101501_create_subjects_table.php

--- database/migrations/2023_09_05_101501_create_subjects_table.php
+++ database/migrations/2023_09_05_101501_create_subjects_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->string("classes");
-            $table->string("description");
+            $table->string("description")->nullable();
             $table->string("status")->default("active");
             $table->timestamps();
         });
