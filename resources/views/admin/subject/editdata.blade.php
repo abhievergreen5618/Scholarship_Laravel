4c7a348a017c0a0ec3b033dcdf85d54cf756555a
shivangi980 <shivangidhiman980@gmail.com>
2023-09-14 09:18:50 +0530

    Update editdata.blade.php

--- resources/views/admin/subject/editdata.blade.php
+++ resources/views/admin/subject/editdata.blade.php
@@ -33,7 +33,8 @@
 @endphp
                         <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" name="classes[]" id="class" multiple="multiple" data-placeholder="Select Classes" data-dropdown-css-class="select2-purple">
                         <option value="{{ $selectedId ?: 'Select Value' }}" selected>{{ $selectedId ?: 'Select Value' }}</option>
-                                @if(!empty($classSelect))
+                                @if(empty($classSelect))
+                                dfghj
                                     @foreach($classSelect as $class)
                                     <option value="{{ $class->class }}">
                                         {{ $class->class }}
