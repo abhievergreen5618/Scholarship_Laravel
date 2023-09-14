34456b71393d464a156b885b26cce34736401052
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-14 11:08:48 +0530

    update

--- resources/views/admin/class/add.blade.php
+++ resources/views/admin/class/add.blade.php
@@ -6,38 +6,34 @@
         <div class="card-header">
             <h3 class="card-title">{{ __('Add Class') }}</h3>
         </div>
-        <!-- /.card-header --> 
-            <form id="class-add-form" action="{{ isset($data) ? route('admin.class.update') : route('admin.class.store') }}" method="POST">
-    @csrf
+        <!-- /.card-header -->
+        <form id="class-add-form" action="{{ isset($data) ? route('admin.class.update') : route('admin.class.store') }}" method="POST">
+            @csrf
 
-    @isset($data)
-    <input type="hidden" name="id" value="{{ encrypt($data->id) }}">
-    @endisset 
+            @isset($data)
+            <input type="hidden" name="id" value="{{ encrypt($data->id) }}">
+            @endisset
 
             <div class="card-body">
                 <div>
                     <div class="form-group">
                         <label>Name</label>
                         <select class="form-control @error('class') {{ 'is-invalid' }} @enderror" id="class" name="class">
-    <option value="">Select Class</option>
-    @for($i=1;$i<=12;$i++)
-        @php
-        if(!isset($data) || $data['class'] != $i) 
-        {
-            $valueExists = DB::table('class_models')->where('class', $i)->exists();
-            if(!$valueExists) {
-                echo "<option value='$i'>$i</option>";
-            }
-        }
-        @endphp
-    @endfor
-</select>
+                            <option value="">Select Class</option>
+                            @for($i=1;$i<=12;$i++) @php if(!isset($data) || $data['class'] !=$i) { $valueExists=DB::table('class_models')->where('class', $i)->exists();
+                                if(!$valueExists) {
+                                echo "<option value='$i'>$i</option>";
+                                }
+                                }
+                                @endphp
+                                @endfor
+                        </select>
 
-@error('class')
-<div>
-    <label class="error fail-alert  mt-1" id="class-error">{{ $message }}</label>
-</div> 
-@enderror
+                        @error('class')
+                        <div>
+                            <label class="error fail-alert  mt-1" id="class-error">{{ $message }}</label>
+                        </div>
+                        @enderror
 
                     </div>
                     <div class="form-group mb-2">
@@ -45,7 +41,7 @@
                         <textarea class="form-control @error('description') {{ 'is-invalid' }} @enderror" id="description" name="description" rows="3" placeholder="Enter ...">{{@old('name',$data->description)}}</textarea>
                     </div>
                     <div class="form-group">
-                       <label for="exampleInputEmail1" class="">{{ __('Status') }}</label>
+                        <label for="exampleInputEmail1" class="">{{ __('Status') }}</label>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="status" id="active" value="active" @isset($data) @if($data['status']=="active" ) {{"checked"}} @endif @endisset>
                             <label class="form-check-label" for="active">Active</label>
