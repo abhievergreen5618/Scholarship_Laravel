e3d9dc00813fb073ff81fbf33bcb16d918f3d92e
shivangi980 <shivangidhiman980@gmail.com>
2023-09-14 12:52:01 +0530

    seeders

--- database/seeders/ClassSeeder.php
+++ database/seeders/ClassSeeder.php
@@ -16,21 +16,21 @@ class ClassSeeder extends Seeder
     public function run()
     {
         //
-        $classes = [
-            ['class' => '1'],
-            ['class' => '2'],
-            ['class' => '3'],
-            ['class' => '4'],
-            ['class' => '5'],
-            ['class' => '6'],
-            ['class' => '7'],
-            ['class' => '8'],
-            ['class' => '9'],
-            ['class' => '10'],
-            ['class' => '11'],
-            ['class' => '12'],
-        ];
+        // $classes = [
+        //     ['class' => '1'],
+        //     ['class' => '2'],
+        //     ['class' => '3'],
+        //     ['class' => '4'],
+        //     ['class' => '5'],
+        //     ['class' => '6'],
+        //     ['class' => '7'],
+        //     ['class' => '8'],
+        //     ['class' => '9'],
+        //     ['class' => '10'],
+        //     ['class' => '11'],
+        //     ['class' => '12'],
+        // ];
 
-        ClassModel::insert($classes);
+        // ClassModel::insert($classes);
     }
 }
