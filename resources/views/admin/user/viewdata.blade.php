34ac80cf62877ac5cad7728c29e2bcde12ef1815
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-14 18:59:11 +0530

    Update viewdata.blade.php

--- resources/views/admin/user/viewdata.blade.php
+++ resources/views/admin/user/viewdata.blade.php
@@ -102,7 +102,7 @@
                                             <tr>
                                                 <td ><b>Gender :</b></td>
                                                 <td>{{(!empty(auth()->user()->step2_updated_at))
-                                                ? auth()->user()->gender : ''}}</td>
+                                                ? auth()->user()->genderSummary : ''}}</td>
                                             </tr>
                                             <tr>
                                                 <td ><b>Applying for subject ? :</b></td>
