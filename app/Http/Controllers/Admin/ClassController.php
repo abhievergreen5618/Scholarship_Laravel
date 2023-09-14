44fb9ba1493b0a798e39c4ec573895d22efad944
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-15 04:30:51 +0530

    Update ClassController.php

--- app/Http/Controllers/Admin/ClassController.php
+++ app/Http/Controllers/Admin/ClassController.php
@@ -21,7 +21,7 @@ class ClassController extends Controller
     public function index()
     {
         // return view('admin.class.index');
-        $class = ClassModel::with('classes')->all();
+        $class = ClassModel::with('classes')->get();
         dd($class);
     }
 
