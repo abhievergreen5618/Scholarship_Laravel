17ef83eed4a51144eff922bee1fa6303de15f4ba
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-15 19:01:59 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -211,7 +211,7 @@ class LoginController extends Controller
             $credentials = $request->only('email','password');
             $remember = $request->has('rememberme');
             if (Auth::attempt($credentials,$remember)) {
-                return redirect()->intended('login');
+                return Auth::user()->role == "admin" ? redirect()->route("admin.dashboard") : redirect()->route("start");
             }
             else
             {
