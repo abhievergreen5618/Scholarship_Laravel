7d7c7f0775b0c0365eb80352565b180129a6420d
shivangi980 <shivangidhiman980@gmail.com>
2023-09-15 13:21:22 +0530

    Update index.blade.php

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -1,35 +1,39 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-<link rel="stylesheet" href="{{ asset('css/tabs.css') }}">
     <title>Document</title>
 </head>
 <body>
-<div class="tab-block" id = "tab-block">
+    
+$(document).ready(function(){
   
-  <ul class="tab-mnu">
-    <li class = "active" >About</li>
-    <li>Team</li>
-    <li>Privacy</li>
-    <li>Advertise</li>
-    <li>Contact</li>
-  </ul>
+  var tabWrapper = $('#tab-block'),
+      tabMnu = tabWrapper.find('.tab-mnu  li'),
+      tabContent = tabWrapper.find('.tab-cont > .tab-pane');
   
-  <div class="tab-cont">
-    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ex optio molestiae deleniti accusantium nobis hic, quia, quaerat ipsa numquam, eius expedita odit earum. Dolores dolore iusto est earum praesentium?</p></div>
-    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, sit, velit mollitia pariatur laudantium officia, fugit neque eligendi saepe sint ea! Debitis aperiam nulla voluptatem libero fuga a, veniam amet.</p></div>
-    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi magnam porro iure fugiat repellendus iusto aperiam ducimus laudantium alias omnis maiores beatae veniam debitis voluptatibus asperiores, voluptates nostrum nihil quae?</p></div>
-    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur excepturi, amet voluptatibus quibusdam assumenda adipisci, quidem ipsa necessitatibus, fugit repellat nam sequi ducimus recusandae id illo? Unde necessitatibus repellat, modi?</p></div>
-    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cupiditate dignissimos quae aspernatur, ex hic, voluptatibus excepturi officia velit dolorum fugiat nam nostrum earum maiores praesentium nemo nobis eaque officiis.</p></div>
-  </div>
-
-</div>
-
-<script src="{{asset('js/tabs.js')}}"></script>
+  tabContent.not(':first-child').hide();
+  
+  tabMnu.each(function(i){
+    $(this).attr('data-tab','tab'+i);
+  });
+  tabContent.each(function(i){
+    $(this).attr('data-tab','tab'+i);
+  });
+  
+  tabMnu.click(function(){
+    var tabData = $(this).data('tab');
+    tabWrapper.find(tabContent).hide();
+    tabWrapper.find(tabContent).filter('[data-tab='+tabData+']').show(); 
+  });
+  
+  $('.tab-mnu > li').click(function(){
+    var before = $('.tab-mnu li.active');
+    before.removeClass('active');
+    $(this).addClass('active');
+   });
+  
+});
 </body>
 </html>
-
