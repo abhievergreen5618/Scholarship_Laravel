1c46591ce8de131889d837a075e505b66b288bdd
shivangi980 <shivangidhiman980@gmail.com>
2023-09-15 17:06:08 +0530

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -83,5 +83,16 @@
 </div>
 </div>
 
+
+<script>
+    $(document).ready(function () {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    });
+</script>
+
   </body>
 </html>
