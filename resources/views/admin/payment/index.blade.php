16033f34a49cacab91982a5a0745a73a7553e4ed
shivangi980 <shivangidhiman980@gmail.com>
2023-09-15 13:18:52 +0530

    tabs

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -9,21 +9,25 @@
     <title>Document</title>
 </head>
 <body>
-<div class="tabs">
-  <ul class="tabs-nav">
-    <li><a href="#tab-1">Features</a></li>
-    <li><a href="#tab-2">Details</a></li>
+<div class="tab-block" id = "tab-block">
+  
+  <ul class="tab-mnu">
+    <li class = "active" >About</li>
+    <li>Team</li>
+    <li>Privacy</li>
+    <li>Advertise</li>
+    <li>Contact</li>
   </ul>
-  <div class="tabs-stage">
-    <div id="tab-1">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec neque nisi, dictum aliquet lectus.</p>
-    </div>
-    <div id="tab-2">
-      <p>Phasellus pharetra aliquet viverra. Donec scelerisque tincidunt diam, eu fringilla urna auctor at.</p>
-    </div>
+  
+  <div class="tab-cont">
+    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ex optio molestiae deleniti accusantium nobis hic, quia, quaerat ipsa numquam, eius expedita odit earum. Dolores dolore iusto est earum praesentium?</p></div>
+    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, sit, velit mollitia pariatur laudantium officia, fugit neque eligendi saepe sint ea! Debitis aperiam nulla voluptatem libero fuga a, veniam amet.</p></div>
+    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi magnam porro iure fugiat repellendus iusto aperiam ducimus laudantium alias omnis maiores beatae veniam debitis voluptatibus asperiores, voluptates nostrum nihil quae?</p></div>
+    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur excepturi, amet voluptatibus quibusdam assumenda adipisci, quidem ipsa necessitatibus, fugit repellat nam sequi ducimus recusandae id illo? Unde necessitatibus repellat, modi?</p></div>
+    <div class="tab-pane"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cupiditate dignissimos quae aspernatur, ex hic, voluptatibus excepturi officia velit dolorum fugiat nam nostrum earum maiores praesentium nemo nobis eaque officiis.</p></div>
   </div>
-</div>
 
+</div>
 
 <script src="{{asset('js/tabs.js')}}"></script>
 </body>
