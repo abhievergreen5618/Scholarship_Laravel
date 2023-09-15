cf0a632d5ae8c4e34c99fa9f409a46819a28ad91
shivangi980 <shivangidhiman980@gmail.com>
2023-09-15 15:11:22 +0530

    Update index.blade.php

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -2,15 +2,7 @@
 <section class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1>Fee</h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Fee</li>
-                </ol>
-            </div>
+            
         </div>
     </div>
 </section>
