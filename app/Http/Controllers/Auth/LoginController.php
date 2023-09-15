7348ca4347bffd0c0ca9f2022cd8597c7b5c7d32
shivangi980 <shivangidhiman980@gmail.com>
2023-09-15 11:12:11 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -206,7 +206,7 @@ class LoginController extends Controller
             $credentials = $request->only('email','password');
             $remember = $request->has('rememberme');
             if (Auth::attempt($credentials,$remember)) {
-                return redirect()->intended('student.form');
+                return redirect()->intended('dashboard');
             }
             else
             {
