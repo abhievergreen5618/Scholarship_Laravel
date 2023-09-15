cd7c360b054e12f712a8bfb90b44904950893aca
shivangi980 <shivangidhiman980@gmail.com>
2023-09-15 23:00:22 +0530

    Update add.blade.php

---
+++ resources/views/admin/user/add.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Users</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Users</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Add User</h3>
+                    </div>
+                    <div class="card-body">
+                        <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                <form id="class-add-form" action="{{route('admin.subject.store') }}" method="POST">
+                                    @csrf
+                                @isset($data)
+                                    <input type="hidden" name="id" value="{{encrypt($data->id)}}">
+                                @endisset
+                     
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+
+@endsection
