597002be4207ed957918147378c5933d6314208d
shivangi980 <shivangidhiman980@gmail.com>
2023-09-15 13:28:06 +0530

    Update index.blade.php

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -1,30 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+@extends('layouts.master')
 
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Fee</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Fee</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Details</h3>
+                    </div>
+                    <div class="card-body">
+                        <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                            <div class="row">
+                                <div class="col-sm-12">
     <link rel="stylesheet" href="{{ asset('css/tabs.css') }}">
-</head>
-<body>
+
     
 <div class="tabs">
   <ul class="tabs-nav">
-    <li><a href="#tab-1">Features</a></li>
-    <li><a href="#tab-2">Details</a></li>
+    <li><a href="#tab-1">Success</a></li>
+    <li><a href="#tab-2">Failure</a></li>
   </ul>
   <div class="tabs-stage">
     <div id="tab-1">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec neque nisi, dictum aliquet lectus.</p>
     </div>
     <div id="tab-2">
-      <p>Phasellus pharetra aliquet viverra. Donec scelerisque tincidunt diam, eu fringilla urna auctor at.</p>
     </div>
   </div>
 </div>
 
 
 <script src="{{asset('js/tabs.js')}}"></script>
-</body>
-</html>
+</table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+@endsection
