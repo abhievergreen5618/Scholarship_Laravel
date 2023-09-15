56276770e690e08ec215c2221ce8353538bdd563
shivangi980 <shivangidhiman980@gmail.com>
2023-09-15 19:34:40 +0530

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
+<link rel="stylesheet" type="text/css" href="{{asset('css/login.css')}}">
 
     <title></title>
   </head>
