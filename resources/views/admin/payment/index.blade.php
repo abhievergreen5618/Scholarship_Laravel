ff767b5e1650c4c639b1df62532bb644e14fcbb2
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-18 18:12:15 +0530

    Update index.blade.php

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -38,20 +38,10 @@
                     <div class="card-body">
                         <div class="tab-content" id="custom-tabs-five-tabContent">
                             <div class="tab-pane fade active show" id="custom-tabs-five-overlay" role="tabpanel" aria-labelledby="custom-tabs-five-overlay-tab">
-                                <div class="overlay-wrapper">
-                                    <div class="overlay"><i class="fas fa-3x fa-sync-alt fa-spin"></i>
-                                        <div class="text-bold pt-2">Loading...</div>
-                                    </div>
                                     @include('admin.payment.allpayment')
-                                </div>
                             </div>
                             <div class="tab-pane fade" id="custom-tabs-five-overlay-dark" role="tabpanel" aria-labelledby="custom-tabs-five-overlay-dark-tab">
-                                <div class="overlay-wrapper">
-                                    <div class="overlay dark"><i class="fas fa-3x fa-sync-alt fa-spin"></i>
-                                        <div class="text-bold pt-2">Loading...</div>
-                                    </div>
                                     @include('admin.payment.success')
-                                </div>
                             </div>
                             <div class="tab-pane fade" id="custom-tabs-five-normal" role="tabpanel" aria-labelledby="custom-tabs-five-normal-tab">
                                 @include('admin.payment.failure')
