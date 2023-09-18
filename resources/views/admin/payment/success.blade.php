07c2a0b2613f93f5021d54a2d2e2a4adec78fcb8
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-18 18:09:39 +0530

    Update success.blade.php

--- resources/views/admin/payment/success.blade.php
+++ resources/views/admin/payment/success.blade.php
@@ -1,11 +1,6 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Details</h3>
-                    </div>
-                    <div class="card-body">
                         <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
                             <div class="row">
                                 <div class="col-sm-12">
@@ -26,9 +21,6 @@
                             </div>
                         </div>
                     </div>
-
-                </div>
-
             </div>
 
         </div>
