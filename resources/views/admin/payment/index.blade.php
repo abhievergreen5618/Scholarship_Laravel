6d81274e3a25478a62706be28f9e60a5711999ae
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-18 16:21:48 +0530

    Update index.blade.php

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container-fluid">
+<div class="container-fluid mt-5">
     <div class="row">
         <div class="col-md-12">
             <div class="card card-primary card-tabs">
