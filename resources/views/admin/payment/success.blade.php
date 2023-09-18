28b4684af0e0a87f5877eac8f64819494cac4901
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-18 17:59:41 +0530

    Update success.blade.php

---
+++ resources/views/admin/payment/success.blade.php
@@ -0,0 +1,39 @@
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Details</h3>
+                    </div>
+                    <div class="card-body">
+                        <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <table style="text-align:center;" id="subjecttable" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Subject Name</th>
+                                                <th>Classes</th>
+                                                <th>Description</th>
+                                                <th>Status</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
