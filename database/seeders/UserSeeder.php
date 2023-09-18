f66f47d7e5ffb2039c0a3d515487ef563e5e4980
abhievergreen5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-09-18 19:46:06 +0530

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,5 +18,12 @@ class UserSeeder extends Seeder
             "role" => "admin",
             "password" => Hash::make("12345678"),
         ]);
+
+        User::create([
+            "name" => "testuser",
+            "email" => "testuser@gmail.com",
+            "role" => "student",
+            "password" => Hash::make("12345678"),
+        ]);
     }
 }
